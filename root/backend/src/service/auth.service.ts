56536e79fd8aa6b3cbbab312b78c1851661f0cd9
asterisktotle <barandajander.gmail.com>
2025-08-13 08:26:19 +0800

    Create auth service create user

--- root/backend/src/service/auth.service.ts
+++ root/backend/src/service/auth.service.ts
@@ -1,13 +1,34 @@
 import { UserModel } from "@/models/user.model"
 import { UserInterface } from "@/types/user.type"
+import crypto from 'crypto'
+
 const createUserAccount = async (userData: UserInterface) => {
-   // TODO:
-   
+// TODO:
+    try {
+        // Check if account is already created
+        const isExistingUser = UserModel.findOne({email: userData.email})
+
+        if(!isExistingUser) {
+            throw new Error('Email is already used')
+        }
+
+        const verificationToken = crypto.randomBytes(32).toString('hex')
+    
+        const user = new UserModel({
+            ...userData,
+            verificationToken,
+            isVerified: false
+        })
+
+        await user.save()
+
+    } catch (error) {
+        throw error
+    }
     
 }
 const loginUserAccount = async () => {
-   // TODO:
-    // 
+   
 }
 const logoutUserAccount = async () => {
    // TODO:
